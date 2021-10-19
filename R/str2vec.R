242180065705712ae1b0de68939757ed5887d290
Ridson <alfrzlp@gmail.com>
2021-10-19 17:01:48 +0700

    s

--- R/str2vec.R
+++ R/str2vec.R
@@ -1,16 +1,16 @@
 #' Merubah string menjadi vektor
 #'
 #' @param str string
-#' @param pemisah character pemisah antar nilai seperti koma dll
+#' @param sep character pemisah antar nilai seperti koma dll
 #'
 #' @return vector
 #' @export
 #'
 #' @examples
 #' str2vec('1.2 3.4 5 9 100')
-#' str2vec('1.2, 3.4, 5, 9, 100', pemisah = '\\,')
+#' str2vec('1.2, 3.4, 5, 9, 100', sep = '\\,')
 
-str2vec <- function (str, pemisah = " "){
+str2vec <- function (str, sep = " "){
     str = gsub(",", ".", str)
     str = gsub(paste0(pemisah, "|\t|\n"), " ", str)
     str = strsplit(str, split = " ")[[1]]
