8ed1d346729820ffbc8e0b61f25713a3d5d4c1c6
ridsonap <alfrzlp@gmail.com>
2021-12-31 16:32:21 +0700

    add data

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Data BPJS Hackathon AI: Case and Cost Prediction.
+#'
+#' A dataset containing the Case, Cost and other attributes of almost 57.971 baris 36 kolom.
+#'
+#' @format A data frame with 57.971 rows and 36 variables:
+#' \describe{
+#'   \item{cost}{cost dari peserta bpjs}
+#'   \item{case}{kasus dari peserta bpjs}
+#'   ...
+#' }
+#' @source \url{https://github.com/rasyidstat/bpai/tree/master/data}
+"bpjs"
